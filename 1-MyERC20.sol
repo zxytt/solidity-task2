4c34a48065f702a4816d72af6ade811eab94654c
zhangxiaoyuan <2405476994@qq.com>
2025-08-04 21:47:08 +0800

    feat:'完善授权功能'

--- 1-MyERC20.sol
+++ 1-MyERC20.sol
@@ -16,9 +16,10 @@ contract MyERC20 {
   address public owner;
 
   // 账户余额
-  mapping(address => uint256) public _balances;
-  // 授权
-  mapping(address => mapping(address => uint256)) public _allowances;
+  mapping(address => uint256) private _balances;
+  // 存储授权信息
+  mapping(address => mapping(address => uint256)) private _allowances;
+
   // 事件
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
@@ -45,10 +46,12 @@ contract MyERC20 {
     _;
   }
 
+  // 查询余额
   function balanceOf(address account) public view returns (uint256) {
     return _balances[account];
   }
 
+  // 转账 
   function transfer(address recipient, uint256 amount) public returns (bool) {
       require(recipient != address(0), "SimpleERC20: transfer to the zero address");
       require(_balances[msg.sender] >= amount, "SimpleERC20: insufficient balance");
@@ -58,6 +61,33 @@ contract MyERC20 {
       
       emit Transfer(msg.sender, recipient, amount);
       return true;
+  }
+
+  // 授权
+  function approve(address spender, uint256 amount) public returns (bool) {
+    _allowances[msg.sender][spender] = amount;
+    emit Approval(msg.sender, spender, amount);
+    return true;
+  }
+
+  // 查询授权
+  function allowance(address owner, address spender) public view returns (uint256) {
+    return _allowances[owner][spender];
+  }
+
+  // 代扣转账功能
+  function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+    require(sender != address(0), "SimpleERC20: transfer from the zero address");
+    require(recipient != address(0), "SimpleERC20: transfer to the zero address");
+    require(_balances[sender] >= amount, "SimpleERC20: insufficient balance");
+    require(_allowances[sender][msg.sender] >= amount, "SimpleERC20: insufficient allowance");
+
+    _balances[sender] -= amount;
+    _balances[recipient] += amount;
+    _allowances[sender][msg.sender] -= amount;
+
+    emit Transfer(sender, recipient, amount);
+    return true;
   }
 
   // 允许合约所有者增发代币
